756df6afae07fe6e75f15ee323d6c6a31199320b
Rakshitha Rathnakar <92865986+rakshitharathnakar@users.noreply.github.com>
2023-08-15 10:13:06 +0530

    Create Add n sub.c

---
+++ Add n sub.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int a=0,b=0;
+    float m=0.0,n=0.0;
+    scanf("%d %d\n %f\n %f",&a,&b,&m,&n);
+    printf("%d %d\n",a,b);
+    printf("%0.1f %0.1f\n",m,n);
+}
